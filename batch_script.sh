3f8eb5e3059b3ba9497785e1e32d520e1bb2c98d
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-06-26 10:28:38 +0200

    Update batch_script.sh

--- batch_script.sh
+++ batch_script.sh
@@ -1,42 +1,43 @@
 #!/bin/bash -l
 
 # SLURM SBATCH Directives
-#SBATCH --job-name=letizias-job
+#SBATCH --job-name=barlow-twins-job
 #SBATCH --time=24:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=12
 #SBATCH --constraint=gpu
-#SBATCH --output=letizias-logfile-%j.log
-#SBATCH --error=letizias-errorfile-%j.err
+#SBATCH --output=barlow-twins-logfile-%j.log
+#SBATCH --error=barlow-twins-errorfile-%j.err
 #SBATCH --account=sd29
 
 # Module loading section
 module load daint-gpu
 module load cray-python
 
+# Activate conda environment
+source activate diaus_1
 
 # Define dataset and labels path
 DATASET_DIR="/store/sdsc/sd29/letizia/sud_corse"
 LABELS_FILE="labels/labels_numeric_binary_MPA_Sud_corse.csv"
 
-
 # Command to run the Python script
 echo "Starting the training process."
-srun -ul $HOME/miniconda3/envs/diaus_1/bin/python self_supervised_pretraining.py \
+srun -ul $HOME/miniconda3/envs/diaus_1/bin/python training_BarlowTwins.py \
     --arg_log True \
     --samples_dir $DATASET_DIR \
     --labels_file $LABELS_FILE \
     --embedder_type barlow_twins \
     --sequence_length 300 \
     --sample_subset_size 500 \
-    --num_classes 2\
+    --num_classes 2 \
     --batch_size 64 \
     --token_emb_dim 4 \
-    --sample_repr_dim 64 \
-    --sample_emb_dim 2 \
-    --barlow_twins_lambda 1 \
+    --sample_repr_dim 128 \
+    --sample_emb_dim 64 \
+    --barlow_twins_lambda 0.05 \
     --initial_learning_rate 1e-3 \
-    --max_epochs 2
+    --max_epochs 3
 
 echo "Training completed successfully."
