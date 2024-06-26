945f2e99dbd7a43cf5c1e8722b57596b0d557aa7
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-06-26 10:31:17 +0200

    Create batch_script_Classifier.sh

---
+++ batch_script_Classifier.sh
@@ -0,0 +1,42 @@
+#!/bin/bash -l
+
+# SLURM SBATCH Directives
+#SBATCH --job-name=classifier-job
+#SBATCH --time=24:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task=12
+#SBATCH --constraint=gpu
+#SBATCH --output=classifier-logfile-%j.log
+#SBATCH --error=classifier-errorfile-%j.err
+#SBATCH --account=sd29
+
+# Module loading section
+module load daint-gpu
+module load cray-python
+
+# Activate conda environment
+source activate diaus_1
+
+# Define dataset and labels path
+DATASET_DIR="/store/sdsc/sd29/letizia/sud_corse"
+LABELS_FILE="labels/labels_numeric_binary_MPA_Sud_corse.csv"
+
+# Command to run the Python script
+echo "Starting the training process."
+srun -ul $HOME/miniconda3/envs/diaus_1/bin/python training_Classifier.py \
+    --arg_log True \
+    --samples_dir $DATASET_DIR \
+    --labels_file $LABELS_FILE \
+    --embedder_type barlow_twins \
+    --sequence_length 300 \
+    --sample_subset_size 500 \
+    --num_classes 2 \
+    --batch_size 64 \
+    --token_emb_dim 4 \
+    --sample_repr_dim 64 \
+    --sample_emb_dim 2 \
+    --initial_learning_rate 1e-3 \
+    --max_epochs 2
+
+echo "Training completed successfully."
