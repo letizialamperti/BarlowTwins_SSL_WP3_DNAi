6a117a6af664a4240cddaad79758146f470e825c
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-06-27 18:09:56 +0200

    Update batch_script_BarlowTwins.sh

--- batch_script_BarlowTwins.sh
+++ batch_script_BarlowTwins.sh
@@ -36,8 +36,8 @@ srun -ul $HOME/miniconda3/envs/diaus_1/bin/python training_BarlowTwins.py \
     --token_emb_dim 8 \
     --sample_repr_dim 128 \
     --sample_emb_dim 64 \
-    --barlow_twins_lambda 0.05 \
+    --barlow_twins_lambda 0.005 \
     --initial_learning_rate 1e-3 \
-    --max_epochs 5
+    --max_epochs 2
 
 echo "Training completed successfully."
