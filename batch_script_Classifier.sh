26570fffee3861c899608ce7084de8958d28ef03
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-08 18:07:52 +0200

    Update batch_script_Classifier.sh

--- batch_script_Classifier.sh
+++ batch_script_Classifier.sh
@@ -37,7 +37,7 @@ srun -ul $HOME/miniconda3/envs/diaus_1/bin/python training_Classifier.py \
     --token_emb_dim 8 \
     --sample_repr_dim 128 \
     --sample_emb_dim 64 \
-    --initial_learning_rate 1e-6 \
+    --initial_learning_rate 1e-5 \
     --max_epochs 1
 
 echo "Training completed successfully."
