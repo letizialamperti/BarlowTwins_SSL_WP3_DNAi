9a313846fd72b9030761da0c103562114acae421
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-11 10:00:45 +0200

    Update batch_script_Classifier.sh

--- batch_script_Classifier.sh
+++ batch_script_Classifier.sh
@@ -19,8 +19,8 @@ module load cray-python
 source activate diaus_1
 
 # Define dataset and labels path
-DATASET_DIR="/store/sdsc/sd29/letizia/sud_corse"
-LABELS_FILE="label/ordinal_label_Sud_Corse.csv"
+DATASET_DIR="/store/sdsc/sd29/letizia/dataset_5_levels_460"
+LABELS_FILE="label/labels_5_levels.csv"
 
 # Command to run the Python script
 echo "Starting the training process."
@@ -32,7 +32,7 @@ srun -ul $HOME/miniconda3/envs/diaus_1/bin/python training_Classifier.py \
     --embedder_type barlow_twins \
     --sequence_length 300 \
     --sample_subset_size 500 \
-    --num_classes 4 \
+    --num_classes 5 \
     --batch_size 32 \
     --token_emb_dim 8 \
     --sample_repr_dim 128 \
