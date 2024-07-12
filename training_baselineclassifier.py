f68215394b06e70b05ad7b6c13b92436c31031f9
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-12 13:34:10 +0200

    Update training_baselineclassifier.py

--- training_baselineclassifier.py
+++ training_baselineclassifier.py
@@ -2,9 +2,10 @@ import torch
 import pytorch_lightning as pl
 from pathlib import Path
 from pytorch_lightning.loggers import WandbLogger
-from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning.callbacks import ModelCheckpoint, EarlyStopping  # Import EarlyStopping
 from ORDNA.data.barlow_twins_datamodule import BarlowTwinsDataModule
-from ORDNA.models.baseline_classifier import BaselineClassifier  # Assicurati che il nome e il percorso siano corretti
+from ORDNA.models.classifier import Classifier
+from ORDNA.models.barlow_twins import SelfAttentionBarlowTwinsEmbedder
 from ORDNA.utils.argparser import get_args, write_config_file
 
 # Usa la stessa configurazione
@@ -14,7 +15,6 @@ if args.arg_log:
 
 pl.seed_everything(args.seed)
 
-print("Setting up data module...")
 samples_dir = Path(args.samples_dir).resolve()
 
 datamodule = BarlowTwinsDataModule(samples_dir=samples_dir,
@@ -23,51 +23,47 @@ datamodule = BarlowTwinsDataModule(samples_dir=samples_dir,
                                    sample_subset_size=args.sample_subset_size,
                                    batch_size=args.batch_size)
 
-# Setup the data module (ensuring that train_dataset is defined)
-datamodule.setup(stage='fit')
-print("Data module setup complete.")
+# Carica il modello Barlow Twins addestrato
+barlow_twins_model = SelfAttentionBarlowTwinsEmbedder.load_from_checkpoint("checkpoints/BT_model_bigdataset-epoch=00.ckpt") #dataset 460
 
-# Calcola l'input_dim basato sui dati
-input_dim = args.sequence_length * args.sample_subset_size
-print(f"Input dimension: {input_dim}")
-
-# Crea il nuovo classificatore
-model = BaselineClassifier(input_dim=input_dim, 
-                           num_classes=args.num_classes, 
-                           initial_learning_rate=args.initial_learning_rate,
-                           train_dataset=datamodule.get_train_dataset())
-
-print("Model created.")
+# Crea il classificatore con il modello Barlow Twins congelato
+model = Classifier(barlow_twins_model=barlow_twins_model, 
+                   sample_repr_dim=args.sample_repr_dim, 
+                   num_classes=args.num_classes, 
+                   initial_learning_rate=args.initial_learning_rate)
 
 # Checkpoint directory
 checkpoint_dir = Path('checkpoints_classifier')
 checkpoint_dir.mkdir(parents=True, exist_ok=True)
-print(f"Checkpoint directory: {checkpoint_dir}")
 
 # General checkpoint callback for best model saving
 checkpoint_callback = ModelCheckpoint(
     monitor='val_accuracy',  # Ensure this metric is logged in your model
     dirpath=checkpoint_dir,
-    filename='baseline_classifier-{epoch:02d}-{val_accuracy:.2f}',
+    filename='big_dataset_classifier-{epoch:02d}-{val_accuracy:.2f}',
     save_top_k=3,
     mode='max',
 )
 
+# Early stopping callback
+early_stopping_callback = EarlyStopping(
+    monitor='val_loss',  # Monitor validation loss
+    patience=100,  # Number of validation steps with no improvement after which training will be stopped
+    mode='min',
+    verbose=True,
+    check_on_train_epoch_end=False  # Check on validation steps
+)
+
 # Setup logger e trainer
 wandb_logger = WandbLogger(project='ORDNA_Class', save_dir=Path("lightning_logs"), config=args, log_model=False)
-print("Wandb logger setup complete.")
-
 trainer = pl.Trainer(
     accelerator='gpu' if torch.cuda.is_available() else 'cpu',
     max_epochs=args.max_epochs,
     logger=wandb_logger,
-    callbacks=[checkpoint_callback],
+    callbacks=[checkpoint_callback, early_stopping_callback],
     log_every_n_steps=10,
     detect_anomaly=False
 )
 
-print("Trainer setup complete.")
-
 # Start training
 trainer.fit(model=model, datamodule=datamodule)
-print("Training started.")
