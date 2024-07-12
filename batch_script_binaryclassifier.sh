3b3d6c804622797c1539080c173a2b24c3465be5
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-12 10:03:38 +0200

    Update batch_script_binaryclassifier.sh

--- batch_script_binaryclassifier.sh
+++ batch_script_binaryclassifier.sh
@@ -20,7 +20,7 @@ source activate diaus_1
 
 # Define dataset and labels path
 DATASET_DIR="/store/sdsc/sd29/letizia/sud_corse"
-LABELS_FILE="label/ordinal_label_Sud_Corse.csv"
+LABELS_FILE="label/labels_numeric_binary_MPA_Sud_corse.csv"
 
 # Command to run the Python script
 echo "Starting the training process."
