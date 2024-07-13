09ee73a44a6c31a45b102167caeba3fef9b19a1b
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-13 16:56:10 +0200

    Update barlow_twins_datamodule.py

--- ORDNA/data/barlow_twins_datamodule.py
+++ ORDNA/data/barlow_twins_datamodule.py
@@ -17,19 +17,23 @@ class BarlowTwinsDataModule(pl.LightningDataModule):
 
     def setup(self, stage: Optional[str] = None) -> None:
         if stage == 'fit' or stage is None:
+            print("Loading train dataset...")
             self.train_dataset = BarlowTwinsDataset(
                 samples_dir=self.train_samples_dir,
                 labels_file=self.labels_file,
                 sample_subset_size=self.sample_subset_size,
                 sequence_length=self.sequence_length
             )
+            print("Train dataset loaded.")
 
+            print("Loading validation dataset...")
             self.val_dataset = BarlowTwinsDataset(
                 samples_dir=self.val_samples_dir,
                 labels_file=self.labels_file,
                 sample_subset_size=self.sample_subset_size,
                 sequence_length=self.sequence_length
             )
+            print("Validation dataset loaded.")
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(
@@ -51,26 +55,21 @@ class BarlowTwinsDataModule(pl.LightningDataModule):
             drop_last=False
         )
 
-
-        
     def calculate_class_weights(self, num_classes):
         print("Calculating class weights...")
-        
-        # Otteniamo tutte le etichette in un solo passaggio
-        labels = [label for _, _, label in self.train_dataset]
-        
-        # Convertiamo le etichette in un tensor di PyTorch
+        labels = []
+        try:
+            for idx, (_, _, label) in enumerate(self.train_dataset):
+                print(f"Processing sample {idx} with label {label}")
+                labels.append(label)
+        except Exception as e:
+            print(f"Error while processing sample {idx}: {e}")
+            raise
         labels = torch.tensor(labels)
-        
-        # Contiamo le occorrenze di ogni classe
+        print(f"All labels: {labels}")
         class_counts = torch.bincount(labels, minlength=num_classes)
-        
-        # Calcoliamo i pesi delle classi
+        print(f"Class counts: {class_counts}")
         class_weights = 1.0 / class_counts.float()
-        
-        # Normalizziamo i pesi
-        class_weights = class_weights / class_weights.sum() * num_classes  
-        
+        class_weights = class_weights / class_weights.sum() * num_classes  # Normalize weights
         print(f"Class weights: {class_weights}")
-        return class_weights    
-
+        return class_weights
