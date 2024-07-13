05e1f4e082dd65d9963115310ec8652b6b3b9575
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-13 16:28:37 +0200

    Update barlow_twins_datamodule.py

--- ORDNA/data/barlow_twins_datamodule.py
+++ ORDNA/data/barlow_twins_datamodule.py
@@ -1,24 +1,15 @@
-import torch
-import os
-import pytorch_lightning as pl
 from torch.utils.data import DataLoader
-from typing import Optional
-from pathlib import Path
-from ORDNA.data.barlow_twins_dataset import BarlowTwinsDataset
-import numpy as np
+import torch
 
 class BarlowTwinsDataModule(pl.LightningDataModule):
     def __init__(self, samples_dir: Path, labels_file: Path, sequence_length: int, sample_subset_size: int, batch_size: int = 8) -> None:
         super().__init__()
-
         self.train_samples_dir = samples_dir / "train"
         self.val_samples_dir = samples_dir / "valid"
         self.labels_file = labels_file
         self.sequence_length = sequence_length
         self.sample_subset_size = sample_subset_size
         self.batch_size = batch_size
-
-        assert(batch_size is not None)
 
     def setup(self, stage: Optional[str] = None) -> None:
         if stage == 'fit' or stage is None:
@@ -35,9 +26,6 @@ class BarlowTwinsDataModule(pl.LightningDataModule):
                 sample_subset_size=self.sample_subset_size,
                 sequence_length=self.sequence_length
             )
-
-    def get_train_dataset(self):
-        return self.train_dataset
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(
@@ -58,3 +46,15 @@ class BarlowTwinsDataModule(pl.LightningDataModule):
             pin_memory=torch.cuda.is_available(), 
             drop_last=False
         )
+
+    def calculate_class_weights(self, num_classes):
+        print("Calculating class weights...")
+        labels = []
+        for _, _, label in self.train_dataset:
+            labels.append(label)
+        labels = torch.tensor(labels)
+        class_counts = torch.bincount(labels, minlength=num_classes)
+        class_weights = 1.0 / class_counts.float()
+        class_weights = class_weights / class_weights.sum() * num_classes  # Normalize weights
+        print(f"Class weights: {class_weights}")
+        return class_weights
