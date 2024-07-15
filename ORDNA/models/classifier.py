672044ee0f1231afca5ec17e455bb0facb928463
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-15 10:59:58 +0200

    Update classifier.py

--- ORDNA/models/classifier.py
+++ ORDNA/models/classifier.py
@@ -30,7 +30,7 @@ class OrdinalCrossEntropyLoss(nn.Module):
         return loss.mean()
 
 class Classifier(pl.LightningModule):
-    def __init__(self, barlow_twins_model: SelfAttentionBarlowTwinsEmbedder, sample_repr_dim: int, num_classes: int, initial_learning_rate: float = 1e-5, class_weights=None):
+    def __init__(self, barlow_twins_model: SelfAttentionBarlowTwinsEmbedder, sample_repr_dim: int, num_classes: int, sequence_length: int, token_emb_dim: int, initial_learning_rate: float = 1e-5, class_weights=None):
         super().__init__()
         print("Initializing Classifier...")
         self.save_hyperparameters(ignore=['barlow_twins_model'])
@@ -41,7 +41,7 @@ class Classifier(pl.LightningModule):
         print("Defining classifier layers...")
         
         # Debug: Check output dimensions of barlow_twins_model
-        dummy_input = torch.randn(1, self.hparams.sequence_length, self.hparams.token_emb_dim).to(self.device)
+        dummy_input = torch.randn(1, sequence_length, token_emb_dim).to(self.device)
         sample_repr = self.barlow_twins_model.repr_module(dummy_input)
         print(f"Sample representation shape: {sample_repr.shape}")
 
