6b071bd31e23e67534fe7f1b3914f38daa273fdb
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-15 11:09:52 +0200

    Update training_Classifier.py

--- training_Classifier.py
+++ training_Classifier.py
@@ -51,7 +51,7 @@ class_weights = calculate_class_weights_from_csv(Path(args.labels_file).resolve(
 
 print("Loading Barlow Twins model...")
 # Carica il modello Barlow Twins addestrato
-barlow_twins_model = SelfAttentionBarlowTwinsEmbedder.load_from_checkpoint("checkpoints/BT_model-epoch=01-v1.ckpt")
+barlow_twins_model = SelfAttentionBarlowTwinsEmbedder.load_from_checkpoint("checkpoints/BT_model_corse-epoch=00.ckpt")
 
 print("Initializing classifier model...")
 # Crea il classificatore con il modello Barlow Twins congelato
