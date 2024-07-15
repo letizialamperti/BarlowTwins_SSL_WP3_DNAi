637f7c937a6f323396396e0772170ba1b0c29797
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-15 13:13:47 +0200

    Update barlow_twins_datamodule.py

--- ORDNA/data/barlow_twins_datamodule.py
+++ ORDNA/data/barlow_twins_datamodule.py
@@ -1,13 +1,16 @@
+import torch
+import os
 import pytorch_lightning as pl
 from torch.utils.data import DataLoader
 from typing import Optional
 from pathlib import Path
 from ORDNA.data.barlow_twins_dataset import BarlowTwinsDataset
-import torch
+import numpy as np
 
 class BarlowTwinsDataModule(pl.LightningDataModule):
     def __init__(self, samples_dir: Path, labels_file: Path, sequence_length: int, sample_subset_size: int, batch_size: int = 8) -> None:
         super().__init__()
+
         self.train_samples_dir = samples_dir / "train"
         self.val_samples_dir = samples_dir / "valid"
         self.labels_file = labels_file
@@ -15,25 +18,32 @@ class BarlowTwinsDataModule(pl.LightningDataModule):
         self.sample_subset_size = sample_subset_size
         self.batch_size = batch_size
 
+        assert(batch_size is not None)
+
     def setup(self, stage: Optional[str] = None) -> None:
         if stage == 'fit' or stage is None:
-            print("Loading train dataset...")
-            self.train_dataset = BarlowTwinsDataset(
-                samples_dir=self.train_samples_dir,
-                labels_file=self.labels_file,
-                sample_subset_size=self.sample_subset_size,
-                sequence_length=self.sequence_length
-            )
-            print("Train dataset loaded. Number of samples:", len(self.train_dataset))
+            if not hasattr(self, 'train_dataset'):
+                print("Loading train dataset...")
+                self.train_dataset = BarlowTwinsDataset(
+                    samples_dir=self.train_samples_dir,
+                    labels_file=self.labels_file,
+                    sample_subset_size=self.sample_subset_size,
+                    sequence_length=self.sequence_length
+                )
+                print(f"Train dataset loaded. Number of samples: {len(self.train_dataset)}")
 
-            print("Loading validation dataset...")
-            self.val_dataset = BarlowTwinsDataset(
-                samples_dir=self.val_samples_dir,
-                labels_file=self.labels_file,
-                sample_subset_size=self.sample_subset_size,
-                sequence_length=self.sequence_length
-            )
-            print("Validation dataset loaded. Number of samples:", len(self.val_dataset))
+            if not hasattr(self, 'val_dataset'):
+                print("Loading validation dataset...")
+                self.val_dataset = BarlowTwinsDataset(
+                    samples_dir=self.val_samples_dir,
+                    labels_file=self.labels_file,
+                    sample_subset_size=self.sample_subset_size,
+                    sequence_length=self.sequence_length
+                )
+                print(f"Validation dataset loaded. Number of samples: {len(self.val_dataset)}")
+
+    def get_train_dataset(self):
+        return self.train_dataset
 
     def train_dataloader(self) -> DataLoader:
         return DataLoader(
