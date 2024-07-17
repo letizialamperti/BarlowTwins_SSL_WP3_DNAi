a0883423d06932c8a603c804d26a27d1e263a15a
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-17 12:29:33 +0200

    Update and rename baseline_classifier.py to classifier_from_scratch.py

---
+++ ORDNA/models/classifier_from_scratch.py
@@ -0,0 +1,91 @@
+import torch
+import torch.nn as nn
+import pytorch_lightning as pl
+from torch.optim import AdamW
+from torchmetrics import Accuracy, ConfusionMatrix, Precision, Recall
+
+class OrdinalCrossEntropyLoss(nn.Module):
+    def __init__(self, num_classes, class_weights=None):
+        super(OrdinalCrossEntropyLoss, self).__init__()
+        self.num_classes = num_classes
+        self.class_weights = class_weights
+
+    def forward(self, logits, labels):
+        logits = logits.to(labels.device)
+        logits = logits.view(-1, self.num_classes)
+        labels = labels.view(-1)
+        cum_probs = torch.sigmoid(logits)
+        cum_probs = torch.cat([cum_probs, torch.ones_like(cum_probs[:, :1])], dim=1)
+        prob = cum_probs[:, :-1] - cum_probs[:, 1:]
+        one_hot_labels = torch.zeros_like(prob).scatter(1, labels.unsqueeze(1), 1)
+        epsilon = 1e-9
+        prob = torch.clamp(prob, min=epsilon, max=1-epsilon)
+        if self.class_weights is not None:
+            class_weights = self.class_weights[labels].view(-1, 1).to(labels.device)
+            loss = - (one_hot_labels * torch.log(prob) + (1 - one_hot_labels) * torch.log(1 - prob)).sum(dim=1) * class_weights
+        else:
+            loss = - (one_hot_labels * torch.log(prob) + (1 - one_hot_labels) * torch.log(1 - prob)).sum(dim=1)
+        return loss.mean()
+
+class ClassifierFromScratch(pl.LightningModule):
+    def __init__(self, input_dim: int, num_classes: int, initial_learning_rate: float = 1e-5, class_weights=None):
+        super().__init__()
+        self.save_hyperparameters()
+        self.num_classes = num_classes
+        
+        self.classifier = nn.Sequential(
+            nn.Linear(input_dim, 256),
+            nn.BatchNorm1d(256),
+            nn.ReLU(),
+            nn.Dropout(0.5),
+            nn.Linear(256, num_classes)
+        )
+        
+        self.class_weights = class_weights.to(self.device) if class_weights is not None else None
+        self.loss_fn = OrdinalCrossEntropyLoss(num_classes, self.class_weights)
+        self.train_accuracy = Accuracy(task="multiclass", num_classes=num_classes).to(self.device)
+        self.val_accuracy = Accuracy(task="multiclass", num_classes=num_classes).to(self.device)
+        self.train_conf_matrix = ConfusionMatrix(task="multiclass", num_classes=num_classes).to(self.device)
+        self.val_conf_matrix = ConfusionMatrix(task="multiclass", num_classes=num_classes).to(self.device)
+        self.train_precision = Precision(task="multiclass", num_classes=num_classes).to(self.device)
+        self.val_precision = Precision(task="multiclass", num_classes=num_classes).to(self.device)
+        self.train_recall = Recall(task="multiclass", num_classes=num_classes).to(self.device)
+        self.val_recall = Recall(task="multiclass", num_classes=num_classes).to(self.device)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        return self.classifier(x)
+
+    def training_step(self, batch, batch_idx: int) -> torch.Tensor:
+        x, labels = batch
+        x, labels = x.to(self.device), labels.to(self.device)
+        output = self(x)
+        class_loss = self.loss_fn(output, labels)
+        self.log('train_class_loss', class_loss, on_step=True, on_epoch=True)
+        pred = torch.argmax(output, dim=1)
+        accuracy = self.train_accuracy(pred, labels)
+        self.log('train_accuracy', accuracy, on_step=True, on_epoch=True)
+        precision = self.train_precision(pred, labels)
+        self.log('train_precision', precision, on_step=True, on_epoch=True)
+        recall = self.train_recall(pred, labels)
+        self.log('train_recall', recall, on_step=True, on_epoch=True)
+        return class_loss
+
+    def validation_step(self, batch, batch_idx: int):
+        x, labels = batch
+        x, labels = x.to(self.device), labels.to(self.device)
+        output = self(x)
+        class_loss = self.loss_fn(output, labels)
+        self.log('val_class_loss', class_loss, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        pred = torch.argmax(output, dim=1)
+        accuracy = self.val_accuracy(pred, labels)
+        self.log('val_accuracy', accuracy, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        precision = self.val_precision(pred, labels)
+        self.log('val_precision', precision, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        recall = self.val_recall(pred, labels)
+        self.log('val_recall', recall, on_step=True, on_epoch=True, prog_bar=True, logger=True)
+        return class_loss
+
+    def configure_optimizers(self):
+        optimizer = AdamW(self.parameters(), lr=self.hparams.initial_learning_rate, weight_decay=1e-4)
+        scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=self.hparams.initial_learning_rate, total_steps=self.trainer.estimated_stepping_batches)
+        return [optimizer], [scheduler]
