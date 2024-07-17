cacb0e393382fb2df4ea0fcb056de55a16ec87c1
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-17 09:55:31 +0200

    Update classifier.py

--- ORDNA/models/classifier.py
+++ ORDNA/models/classifier.py
@@ -102,3 +102,4 @@ class Classifier(pl.LightningModule):
         optimizer = AdamW(self.parameters(), lr=self.hparams.initial_learning_rate, weight_decay=1e-4)
         scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=self.hparams.initial_learning_rate, total_steps=self.trainer.estimated_stepping_batches)
         return [optimizer], [scheduler]
+
