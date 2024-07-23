844c6f194cc12f1018f18486def033f16fd04d7b
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-23 14:52:56 +0200

    Update batch_script_corse_classifier.sh

--- batch_script_corse_classifier.sh
+++ batch_script_corse_classifier.sh
@@ -39,7 +39,7 @@ srun -ul $HOME/miniconda3/envs/diaus_1/bin/python training_Classifier.py \
     --token_emb_dim 8 \
     --sample_repr_dim 64 \
     --sample_emb_dim 64 \
-    --initial_learning_rate 1e-3 \
+    --initial_learning_rate 1e-4 \
     --max_epochs 2
 
 echo "Training completed successfully."
