4f99642230dbc3f9bcb215d857de9bb0f6ea0d36
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-25 21:25:52 +0200

    Update classifier.py

--- ORDNA/models/classifier.py
+++ ORDNA/models/classifier.py
@@ -2,8 +2,7 @@ import torch
 import torch.nn as nn
 import pytorch_lightning as pl
 from torch.optim import AdamW
-from torchmetrics import Accuracy, ConfusionMatrix, Precision, Recall
-from ORDNA.models.barlow_twins import SelfAttentionBarlowTwinsEmbedder
+from torchmetrics import Accuracy, Precision, Recall, MeanAbsoluteError, MeanSquaredError
 
 class OrdinalCrossEntropyLoss(nn.Module):
     def __init__(self, num_classes, class_weights=None):
@@ -25,20 +24,17 @@ class OrdinalCrossEntropyLoss(nn.Module):
             class_weights = self.class_weights[labels].view(-1, 1).to(labels.device)
             loss = - (one_hot_labels * torch.log(prob) + (1 - one_hot_labels) * torch.log(1 - prob)).sum(dim=1) * class_weights
         else:
-            loss = - (one_hot_labels * torch.log(prob) + (1 - one_hot_labels) * torch.log(1 - prob)).sum(dim=1)
+            loss = - (one_hot_labels * torch.log(prob) + (1 - one-hot_labels) * torch.log(1 - prob)).sum(dim=1)
         return loss.mean()
 
 class Classifier(pl.LightningModule):
-    def __init__(self, barlow_twins_model: SelfAttentionBarlowTwinsEmbedder, sample_emb_dim: int, num_classes: int, initial_learning_rate: float = 1e-5, class_weights=None):
+    def __init__(self, sample_emb_dim: int, num_classes: int, num_habitats: int, initial_learning_rate: float = 1e-5, class_weights=None):
         super().__init__()
-        self.save_hyperparameters(ignore=['barlow_twins_model'])
-        self.barlow_twins_model = barlow_twins_model.eval()
+        self.save_hyperparameters()
         self.num_classes = num_classes
-        for param in self.barlow_twins_model.parameters():
-            param.requires_grad = False
-        
+
         self.classifier = nn.Sequential(
-            nn.Linear(sample_emb_dim, 256),
+            nn.Linear(sample_emb_dim + num_habitats, 256),
             nn.BatchNorm1d(256),
             nn.ReLU(),
             nn.Dropout(0.5),
@@ -49,59 +45,68 @@ class Classifier(pl.LightningModule):
         self.loss_fn = OrdinalCrossEntropyLoss(num_classes, self.class_weights)
         self.train_accuracy = Accuracy(task="multiclass", num_classes=num_classes).to(self.device)
         self.val_accuracy = Accuracy(task="multiclass", num_classes=num_classes).to(self.device)
-        self.train_conf_matrix = ConfusionMatrix(task="multiclass", num_classes=num_classes).to(self.device)
-        self.val_conf_matrix = ConfusionMatrix(task="multiclass", num_classes=num_classes).to(self.device)
         self.train_precision = Precision(task="multiclass", num_classes=num_classes).to(self.device)
         self.val_precision = Precision(task="multiclass", num_classes=num_classes).to(self.device)
         self.train_recall = Recall(task="multiclass", num_classes=num_classes).to(self.device)
         self.val_recall = Recall(task="multiclass", num_classes=num_classes).to(self.device)
+        self.train_mae = MeanAbsoluteError().to(self.device)
+        self.val_mae = MeanAbsoluteError().to(self.device)
+        self.train_mse = MeanSquaredError().to(self.device)
+        self.val_mse = MeanSquaredError().to(self.device)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        sample_emb = self.barlow_twins_model(x.to(self.device))  # Utilizza l'output completo del modello Barlow Twins
-        return self.classifier(sample_emb)
+        return self.classifier(x)
 
     def training_step(self, batch, batch_idx: int) -> torch.Tensor:
-        sample_subset1, sample_subset2, labels = batch
-        sample_subset1, sample_subset2, labels = sample_subset1.to(self.device), sample_subset2.to(self.device), labels.to(self.device)
-        output1 = self(sample_subset1)
-        output2 = self(sample_subset2)
-        class_loss = self.loss_fn(output1, labels) + self.loss_fn(output2, labels)
+        embeddings, habitats, labels = batch
+        combined_input = torch.cat((embeddings, habitats), dim=1)
+
+        output = self(combined_input)
+        labels = labels.to(self.device)
+        class_loss = self.loss_fn(output, labels)
+        
         self.log('train_class_loss', class_loss)
-        pred1 = torch.argmax(output1, dim=1)
-        pred2 = torch.argmax(output2, dim=1)
-        combined_preds = torch.cat((pred1, pred2), dim=0)
-        combined_labels = torch.cat((labels, labels), dim=0)
-        accuracy = self.train_accuracy(combined_preds, combined_labels)
+        pred = torch.argmax(output, dim=1)
+        accuracy = self.train_accuracy(pred, labels)
         self.log('train_accuracy', accuracy)
-        precision = self.train_precision(combined_preds, combined_labels)
+        precision = self.train_precision(pred, labels)
         self.log('train_precision', precision)
-        recall = self.train_recall(combined_preds, combined_labels)
+        recall = self.train_recall(pred, labels)
         self.log('train_recall', recall)
+        mae = self.train_mae(pred, labels)
+        self.log('train_mae', mae)
+        mse = self.train_mse(pred, labels)
+        self.log('train_mse', mse)
         return class_loss
 
     def validation_step(self, batch, batch_idx: int) -> torch.Tensor:
-        sample_subset1, sample_subset2, labels = batch
-        sample_subset1, sample_subset2, labels = sample_subset1.to(self.device), sample_subset2.to(self.device), labels.to(self.device)
-        output1 = self(sample_subset1)
-        output2 = self(sample_subset2)
-        class_loss = self.loss_fn(output1, labels) + self.loss_fn(output2, labels)
+        embeddings, habitats, labels = batch
+        combined_input = torch.cat((embeddings, habitats), dim=1)
+
+        output = self(combined_input)
+        labels = labels.to(self.device)
+        class_loss = self.loss_fn(output, labels)
+        
         self.log('val_class_loss', class_loss)  # Log without specifying on_step or on_epoch
-        pred1 = torch.argmax(output1, dim=1)
-        pred2 = torch.argmax(output2, dim=1)
-        combined_preds = torch.cat((pred1, pred2), dim=0)
-        combined_labels = torch.cat((labels, labels), dim=0)
-        accuracy = self.val_accuracy(combined_preds, combined_labels)
+        self.log('val_class_loss_step', class_loss)  # Log on step specifically
+        pred = torch.argmax(output, dim=1)
+        accuracy = self.val_accuracy(pred, labels)
         self.log('val_accuracy', accuracy)
-        precision = self.val_precision(combined_preds, combined_labels)
+        self.log('val_accuracy_step', accuracy)  # Log on step specifically
+        precision = self.val_precision(pred, labels)
         self.log('val_precision', precision)
-        recall = self.val_recall(combined_preds, combined_labels)
+        recall = self.val_recall(pred, labels)
         self.log('val_recall', recall)
+        mae = self.val_mae(pred, labels)
+        self.log('val_mae', mae)
+        mse = self.val_mse(pred, labels)
+        self.log('val_mse', mse)
         return class_loss
 
     def configure_optimizers(self):
         optimizer = AdamW(self.parameters(), lr=self.hparams.initial_learning_rate, weight_decay=1e-4)
         scheduler = {
             'scheduler': torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, mode='min', factor=0.1, patience=5, verbose=True),
-            'monitor': 'val_class_loss'
+            'monitor': 'val_class_loss_step'
         }
         return [optimizer], [scheduler]
