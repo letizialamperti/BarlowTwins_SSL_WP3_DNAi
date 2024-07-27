afd36fb013025d874ce77aaf236bb361b828fd04
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2024-07-27 23:13:54 +0200

    Update batch_script_BarlowTwins.sh

--- batch_script_BarlowTwins.sh
+++ batch_script_BarlowTwins.sh
@@ -20,8 +20,8 @@ source activate diaus_1
 
 
 # Define dataset and labels path
-DATASET_DIR="/store/sdsc/sd29/letizia/dataset_5_levels_460"
-LABELS_FILE="label/labels_5_levels.csv"
+DATASET_DIR="/store/sdsc/sd29/letizia/sud_corse"
+LABELS_FILE="label/ordinal_label_Sud_Corse.csv"
 
 
 # Command to run the Python script
@@ -33,7 +33,7 @@ srun -ul $HOME/miniconda3/envs/diaus_1/bin/python training_BarlowTwins.py \
     --embedder_type barlow_twins \
     --sequence_length 300 \
     --sample_subset_size 500 \
-    --num_classes 5 \
+    --num_classes 4 \
     --batch_size 32 \
     --token_emb_dim 8 \
     --sample_repr_dim 64 \
