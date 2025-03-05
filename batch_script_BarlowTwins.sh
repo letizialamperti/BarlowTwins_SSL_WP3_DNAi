2bbb4a9619700ff1133a43323928d1a10c3cf798
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2025-03-05 15:26:43 +0100

    Update batch_script_BarlowTwins.sh

--- batch_script_BarlowTwins.sh
+++ batch_script_BarlowTwins.sh
@@ -1,42 +1,35 @@
-#!/bin/bash -l
+#!/bin/bash
+set -e  # Ferma il job in caso di errore
 
-# SLURM SBATCH Directives
-#SBATCH --job-name=barlow-twins-job
-#SBATCH --time=24:00:00
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task=12
-#SBATCH --constraint=gpu
-#SBATCH --output=barlow-twins-logfile-%j.log
-#SBATCH --error=barlow-twins-errorfile-%j.err
-#SBATCH --account=sd29
-
-# Module loading section
-module load daint-gpu
-module load cray-python
-
-# Activate conda environment
-source activate diaus_1
+#OAR -n barlow-twins-job
+#OAR -l /nodes=1/gpu=1/core=12,walltime=24:00:00
+#OAR -p gpumodel='A100'
+#OAR --stdout barlow-twins-logfile.out
+#OAR --stderr barlow-twins-errorfile.err
+#OAR --project pr-qiepb
 
 
-# Define dataset and labels path
-DATASET_DIR="/store/sdsc/sd29/letizia/cote_france"
-LABELS_FILE="label/labels_5_cote_france.csv"
+# Attivare Conda
+source /applis/environments/conda.sh
+conda activate zioboia
 
+# Definire percorsi dataset e labels
+DATASET_DIR="/bettik/PROJECTS/pr-qiepb/lampertl"
+LABELS_FILE="label/labels_5_levels.csv"
 
-# Command to run the Python script
+# Eseguire lo script Python
 echo "Starting the training process."
-srun -ul $HOME/miniconda3/envs/diaus_1/bin/python training_BarlowTwins.py \
+python training_BarlowTwins.py \
     --arg_log True \
     --samples_dir $DATASET_DIR \
     --labels_file $LABELS_FILE \
     --embedder_type barlow_twins \
     --sequence_length 300 \
     --sample_subset_size 500 \
-    --num_classes 4 \
+    --num_classes 5 \
     --batch_size 32 \
     --token_emb_dim 8 \
-    --sample_repr_dim 128 \
+    --sample_repr_dim 64 \
     --sample_emb_dim 2 \
     --barlow_twins_lambda 1 \
     --initial_learning_rate 1e-3 \
