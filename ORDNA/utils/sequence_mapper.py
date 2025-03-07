673217b6d4fcce11487f51a4a22856a85bc64801
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2025-03-07 15:59:41 +0100

    Update sequence_mapper.py

--- ORDNA/utils/sequence_mapper.py
+++ ORDNA/utils/sequence_mapper.py
@@ -1,18 +1,28 @@
 from Bio.Data import IUPACData
 from typing import Optional, List
+import math
 
 class SequenceMapper():
     def __init__(self) -> None:
         iupac_letters = IUPACData.ambiguous_dna_letters.lower()
-        iupac_letters_indices = [*range(1, len(iupac_letters)+1)]
+        iupac_letters_indices = list(range(1, len(iupac_letters)+1))
 
         self.iupac_dict = dict(zip(iupac_letters, iupac_letters_indices))
         self.iupac_dict["pad"] = 0
-        self.iupac_dict["u"] = len(iupac_letters)+1 # u is missing in IUPACData for some reason
+        self.iupac_dict["u"] = len(iupac_letters) + 1  # u is missing in IUPACData for some reason
 
     def map_seq(self, seq: str, pad_to_len: Optional[int] = None) -> List[int]:
-        mapped_seq = [self.iupac_dict[nucleotide.lower()] for nucleotide in seq]
-        if not pad_to_len:
+        # Gestisci il caso in cui seq non sia una stringa (ad esempio, sia NA o altro)
+        if not isinstance(seq, str):
+            # Se è un float e risulta NaN, lo sostituisco con una stringa vuota
+            if isinstance(seq, float) and math.isnan(seq):
+                seq = ""
+            else:
+                seq = str(seq)
+                
+        # Mappa ogni nucleotide in minuscolo
+        mapped_seq = [self.iupac_dict[nucleotide.lower()] for nucleotide in seq if nucleotide]
+        if pad_to_len is None:
             return mapped_seq
         elif pad_to_len - len(mapped_seq) > 0:
             mapped_seq = mapped_seq + [self.iupac_dict["pad"]] * (pad_to_len - len(mapped_seq))
