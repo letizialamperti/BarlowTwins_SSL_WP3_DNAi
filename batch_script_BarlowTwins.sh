c8a3cd0d3a124e2d65ff393da40cf81be7cbc8e1
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2025-03-13 09:38:03 +0100

    Update batch_script_BarlowTwins.sh

--- batch_script_BarlowTwins.sh
+++ batch_script_BarlowTwins.sh
@@ -33,6 +33,6 @@ python training_BarlowTwins.py \
     --sample_emb_dim 64 \
     --barlow_twins_lambda 0.005 \
     --initial_learning_rate 1e-3 \
-    --max_epochs 100
+    --max_epochs 1
 
 echo "Training completed successfully."
