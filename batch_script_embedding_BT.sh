205c82029d9fa67968cb626e51c03839ba2d0d33
Letizia Lamperti <51125115+letizialamperti@users.noreply.github.com>
2025-03-17 11:08:29 +0100

    Update batch_script_embedding_BT.sh

--- batch_script_embedding_BT.sh
+++ batch_script_embedding_BT.sh
@@ -5,7 +5,7 @@ set -e  # Ferma lo script se si verifica un errore
 # DIRETTIVE SCHEDULING (modifica se usi OAR, SLURM, ecc.)
 # Esempio con direttive OAR (commenta o modifica se non necessario)
 #OAR -n embedding-job
-#OAR -l /nodes=1/gpu=1/core=12,walltime=02:00:00
+#OAR -l /nodes=1/gpu=1/core=12,walltime=24:00:00
 #OAR -p gpumodel='A100'
 #OAR --stdout embedding-logfile.out
 #OAR --stderr embedding-errorfile.err
